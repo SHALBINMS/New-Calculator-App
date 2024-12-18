98086846a4167b027cd035f0ea1b1bb4dc5c2906
shalbinms88@gmail.com <shalbinms88@gmail.com>
2024-12-18 12:02:00 +0530

    Hello World

--- app/src/main/java/com/example/newcalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/newcalculatorapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.newcalculatorapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    Button bt1, bt2, bt3, bt4;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        bt1 = (Button) findViewById(R.id.addbtn);
+        bt2 = (Button) findViewById(R.id.subbtn);
+        bt3 = (Button) findViewById(R.id.multbtn);
+        bt4 = (Button) findViewById(R.id.divbtn);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob1 = new Intent(getApplicationContext(), AdditionActivity.class);
+                startActivity(ob1);
+            }
+        });
+
     }
 }
+
+
